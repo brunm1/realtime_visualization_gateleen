44d7fd51f4c370eb49234a97d4600488fee62192
brunm1 <marcdenis.bruenisholz@students.bfh.ch>
2017-10-25 21:02:32 +0200

    Add skeleton for end to end test

---
+++ gapa-test/src/test/java/End2EndTest.java
@@ -0,0 +1,9 @@
+import ch.bfh.ti.gapa.cli.Cli;
+import org.junit.jupiter.api.Test;
+
+public class End2EndTest {
+    @Test
+    public void end2endTest() {
+        Cli.main(new String[]{});
+    }
+}
