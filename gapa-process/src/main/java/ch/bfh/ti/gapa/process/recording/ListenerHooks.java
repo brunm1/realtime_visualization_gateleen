03ad4c1fa3b72316d24199d3d13ea845e14de3f9
Adrian Aulbach <adrian@aulbach.ch>
2017-10-27 08:43:52 +0200

    add class ListenerHooks

---
+++ gapa-process/src/main/java/ch/bfh/ti/gapa/process/recording/ListenerHooks.java
@@ -0,0 +1,13 @@
+package ch.bfh.ti.gapa.process.recording;
+
+import java.util.regex.Pattern;
+
+public class ListenerHooks {
+    private Pattern urlPattern;
+    private Pattern filter;
+    private String destination;
+    private String[] methods;
+    private boolean fullUrl;
+
+
+}
