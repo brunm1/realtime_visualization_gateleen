3ac56b684c39577dcfa5abc69a985007f997b005
brunm1 <marcdenis.bruenisholz@students.bfh.ch>
2017-11-10 16:30:22 +0100

    escape actor names in plantuml

--- gapa-process/src/main/java/ch/bfh/ti/gapa/process/diagram/SequenceDiagramGenerator.java
+++ gapa-process/src/main/java/ch/bfh/ti/gapa/process/diagram/SequenceDiagramGenerator.java
@@ -9,8 +9,25 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class SequenceDiagramGenerator {
+
+    private Pattern simpleActorNamePattern = Pattern.compile("^[a-zA-Z][a-zA-Z0-9]*$");
+
+    private boolean doesActorNameNeedEscaping(String actorName) {
+        Matcher matcher = simpleActorNamePattern.matcher(actorName);
+        return !matcher.find();
+    }
+
+    private String escapeActorNameIfNeeded(String actorName) {
+        if(doesActorNameNeedEscaping(actorName)) {
+            return "\"" + actorName + "\"";
+        } else {
+            return actorName;
+        }
+    }
 
     public String generatePlantUmlSequenceDiagram(List<SequenceDiagramMessage> messages) {
         StringBuilder sb = new StringBuilder();
@@ -22,9 +39,9 @@ public class SequenceDiagramGenerator {
         //iterates over all messages and generates a line that depicts it
         for(SequenceDiagramMessage message : messages) {
             sb
-                .append(message.getFrom())
+                .append(escapeActorNameIfNeeded(message.getFrom()))
                 .append(" -> ")
-                .append(message.getTo())
+                .append(escapeActorNameIfNeeded(message.getTo()))
                 .append(" : ")
                 .append(message.getLabel())
                 .append("\n");
